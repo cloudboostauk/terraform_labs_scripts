a558627413f041c93434c6523defb805a610ce42
Akinwunmi Akinrimisi <78056848+akinwunmi-akinrimisi@users.noreply.github.com>
2023-05-13 14:28:12 +0100

    Update install_apache.sh

--- lab_01/install_apache.sh
+++ lab_01/install_apache.sh
@@ -1,7 +1,9 @@
 
 #!/bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo service httpd start
-sudo service httpd status
+sudo su
+yum update -y
+yum install httpd -y
+systemctl start httpd.service
+systemctl enable httpd.service
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+
