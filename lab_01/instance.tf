a74511cf05a2325bd85e81acb142436f039e4a1e
Akinwunmi Akinrimisi <78056848+akinwunmi-akinrimisi@users.noreply.github.com>
2023-10-14 11:44:16 +0100

    Update instance.tf

--- lab_01/instance.tf
+++ lab_01/instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "cba_tf_instance" {
   instance_type = var.instance_type
   security_groups = [aws_security_group.cba_tf_sg.name]
   ami = var.instance_ami
-  key_name = data.aws_key_pair.sample_kp.key_name
+  key_name = var.key_name
 
   tags = {
     Name = "CBATerraformInstance"
